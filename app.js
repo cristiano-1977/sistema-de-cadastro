98709fb668d99210edcb3b13d56f68dc106a3ea1
Cristiano Ferreira da Silva <toshiekuri1977@gmail.com>
2022-06-28 05:36:20 +0900

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação estáv se conectando ao banco de dados")
+console.log("Fizesmo esse commit sem voce ver na aula!")
+console,log("Enviando para o GitHub")
+
+
